b20bf66e52ba9cf945e93f461b44d9b7face665a
vsriprasath <31073266+vsriprasath@users.noreply.github.com>
2018-04-15 16:11:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.send('Article one was requested and it will be served here');
+});
+app.get('/article-two', function (req, res) {
+  res.send('Article Two was requested and it will be served here');
+});
+app.get('/article-three', function (req, res) {
+  res.send('Article Three was requested and it will be served here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
